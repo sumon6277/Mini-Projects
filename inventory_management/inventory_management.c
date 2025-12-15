3e00eb27dbe80cc4aeb94cb3b780b3af806927b4
Sumon-Ahmed <sumonahmed72953@gmail.com>
2025-12-15 19:48:33 +0600

    Implemented Add to card, payment, report . Inventory management mini project completed.

--- inventory_management/inventory_management.c
+++ inventory_management/inventory_management.c
@@ -22,37 +22,48 @@ struct uProduct
     float price;
 };
 
+struct User
+{
+    int id;
+    struct Product p;
+    float total;
+    float payment;
+};
+
 struct Product p;
-struct uProduct up = {0};
+struct uProduct up;
+struct User u;
 
 int found = 0;
+int payment_done = 0;
 
 pthread_mutex_t lock;
 sem_t sem;
 
-void settingAdminLogInfo();
-void inputPassword(char *password, int size);
-void adminLog();
-void setColor(WORD color);
-void userLog();
+void setColor();
 void mainMenu();
+void adminLog();
+void settingAdminLogInfo();
+void userLog();
+void inputPassword();
+void adminView();
+void userView();
 void addProduct();
-void viewProduct();
-void updateProduct();
-void searchProduct();
-void removeProduct();
 void* inputProductThread();
 void* addProductThread();
 void updateProduct();
 void* inputUpdateThread();
 void* addUpdateThread();
-void adminView();
 void searchProduct();
 void* inputSearchProduct();
 void* viewSearchProduct();
 void deleteProduct();
 void* inputDeleteProduct();
 void* addDeleteProduct();
+void purchaseProduct();
+void payment();
+void* updateInventory(void* args);
+void* storeSalesProduct();
 
 HANDLE h;
 
@@ -61,6 +72,9 @@ FILE *userlog;
 FILE *inventory;
 FILE *purchase;
 FILE *temp;
+FILE *userpurchase;
+FILE *usertemp;
+FILE *sales;
 
 bool updateName;
 bool updatePrice;
@@ -85,11 +99,14 @@ void* inputProductThread(void* arg)
     printf("\nEnter productID : ");
     scanf("%d",&p.id);
     getchar();
+
     printf("\nEnter product name : ");
     fgets(p.name,sizeof(p.name),stdin);
     p.name[strcspn(p.name,"\n")] = '\0';
+
     printf("\nEnter product quantity : ");
     scanf("%d",&p.quantity);
+
     printf("\nEnter product price : ");
     scanf("%f",&p.price);
 
@@ -105,6 +122,12 @@ void* addProductThread(void* arg)
     pthread_mutex_lock(&lock);
 
     inventory = fopen("inventory.txt","a");
+    if(!inventory)
+    {
+        printf("No product found\n");
+        return NULL;
+    }
+
     fprintf(inventory,"%d %s %d %.2f\n",p.id,p.name,p.quantity,p.price);
     fclose(inventory);
 
@@ -135,6 +158,12 @@ void* inputUpdateThread(void* arg)
     printf("\nEnter productID to be updated : ");
     scanf("%d",&up.id);
     inventory = fopen("inventory.txt","r");
+
+    if(!inventory)
+    {
+        printf("No product found\n");
+        return NULL;
+    }
 
     while(fscanf(inventory,"%d %s %d %f",&p.id,p.name,&p.quantity,&p.price) != EOF)
     {
@@ -167,6 +196,7 @@ void* inputUpdateThread(void* arg)
                 fgets(up.name,sizeof(up.name),stdin);
                 up.name[strcspn(up.name,"\n")] = '\0';
                 updateName = true;
+
                 printf("Enter product new price : ");
                 scanf("%f",&up.price);
                 updatePrice = true;
@@ -192,13 +222,17 @@ void* addUpdateThread(void* arg)
     inventory = fopen("inventory.txt","r");
     if(!inventory)
     {
+        printf("No product found\n");
         return NULL;
     }
+
     temp = fopen("temp.txt","w");
     if(!temp)
     {
+        printf("A simple error occured\n");
         return NULL;
     }
+
     while(fscanf(inventory,"%d %s %d %f",&p.id,p.name,&p.quantity,&p.price) != EOF)
     {
         if(up.id == p.id)
@@ -228,21 +262,23 @@ void* addUpdateThread(void* arg)
     remove("inventory.txt");
     rename("temp.txt","inventory.txt");
     pthread_mutex_unlock(&lock);
+    
+    return NULL;
 }
 
 void viewProduct() {
-     inventory = fopen("inventory.txt", "r");
+    inventory = fopen("inventory.txt", "r");
 
-    if (!inventory) {
-        printf("No products!\n");
-        return;
+    if(!inventory)
+    {
+        printf("No product found\n");
+        return NULL;
     }
 
     printf("\nID\tName\tQty\tPrice\n");
-    while (fscanf(inventory, "%d %s %d %f",
-                  &p.id, p.name, &p.quantity, &p.price) != EOF) {
-        printf("%d\t%s\t%d\t%.2f\n",
-               p.id, p.name, p.quantity, p.price);
+    while (fscanf(inventory, "%d %s %d %f",&p.id, p.name, &p.quantity, &p.price) != EOF) 
+    {
+        printf("%d\t%s\t%d\t%.2f\n",p.id, p.name, p.quantity, p.price);
     }
     fclose(inventory);
 }
@@ -250,11 +286,14 @@ void viewProduct() {
 void searchProduct()
 {
     pthread_t t1,t2;
+
     pthread_create(&t1,NULL,inputSearchProduct,NULL);
     pthread_create(&t2,NULL,viewSearchProduct,NULL);
 
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
+
+    return;
 }
 void* inputSearchProduct(void* args)
 {
@@ -269,6 +308,12 @@ void* viewSearchProduct(void* args)
     sem_wait(&sem);
     inventory = fopen("inventory.txt", "r");
 
+    if(!inventory)
+    {
+        printf("No product found\n");
+        return NULL;
+    }
+
     while (fscanf(inventory, "%d %s %d %f",&p.id, p.name, &p.quantity, &p.price) != EOF) {
         if (p.id == up.id) {
             printf("Product-Found: %s Quantity:%d Price:%.2f\n",p.name, p.quantity, p.price);
@@ -280,7 +325,9 @@ void* viewSearchProduct(void* args)
     }
 
     if (found == 0)
+    {
         printf("Product not found!\n");
+    }
 
     fclose(inventory);
     return NULL;
@@ -289,6 +336,7 @@ void* viewSearchProduct(void* args)
 void deleteProduct() 
 {
     pthread_t t1,t2;
+
     pthread_create(&t1,NULL,inputDeleteProduct,NULL);
     pthread_create(&t2,NULL,addDeleteProduct,NULL);
 
@@ -296,6 +344,8 @@ void deleteProduct()
     pthread_join(t2,NULL);
     
     printf("Product Deleted!\n");
+
+    return;
 }
 
 void* inputDeleteProduct(void* args)
@@ -314,7 +364,20 @@ void* addDeleteProduct(void* args)
     pthread_mutex_lock(&lock);
 
     inventory = fopen("inventory.txt", "r");
+
+    if(!inventory)
+    {
+        printf("No product found\n");
+        return NULL;
+    }
+
     temp = fopen("temp.txt", "w");
+
+    if(!temp)
+    {
+        printf("A simple error occured\n");
+        return NULL;
+    }
 
     while (fscanf(inventory, "%d %s %d %f",&p.id, p.name, &p.quantity, &p.price) != EOF) {
         if (p.id != up.id)
@@ -325,10 +388,228 @@ void* addDeleteProduct(void* args)
 
     fclose(inventory);
     fclose(temp);
+
     remove("inventory.txt");
     rename("temp.txt", "inventory.txt");
+
     pthread_mutex_unlock(&lock);
+
+    return NULL;
 }
+
+void purchaseProduct()
+{
+    pthread_t t1,t2,t3;
+    found = 0;
+    payment_done = 0;
+
+    u.id = rand()%5 + 1;
+    
+    printf("Enter productID to make purchase: ");
+    scanf("%d",&u.p.id);
+
+    printf("\nEnter quantity : ");
+    scanf("%d",&u.p.quantity);
+
+    pthread_mutex_lock(&lock);
+    inventory = fopen("inventory.txt","r");
+    
+    if(!inventory)
+    {
+        printf("No product found\n");
+        return NULL;
+    }
+
+    while (fscanf(inventory, "%d %s %d %f",&p.id, p.name, &p.quantity, &p.price) != EOF) 
+    {
+        if (p.id == u.p.id && p.quantity >= u.p.quantity) {
+            found = 1;
+            strcpy(u.p.name, p.name);
+            u.total = u.p.quantity * p.price;
+            break;
+        }
+    }
+    
+    fclose(inventory);
+    pthread_mutex_unlock(&lock);
+
+    if (!found) {
+        printf("Insufficient stock or product not found!\n");
+        return;
+    }
+
+    pthread_mutex_lock(&lock);
+    userpurchase = fopen("userpurchase.txt","a");
+
+    if(!userpurchase)
+    {
+        printf("No product is sold\n");
+        return NULL;
+    }
+
+    fprintf(userpurchase, "%d %s %d %.2f\n", u.id, u.p.name, u.p.quantity, u.total);
+
+    fclose(userpurchase);
+    pthread_mutex_unlock(&lock);
+
+    payment();
+
+    if (!payment_done) {
+        printf("Payment not completed. Purchase terminated.\n");
+        return;
+    }
+    pthread_create(&t2,NULL,updateInventory,NULL);
+    pthread_create(&t3,NULL,storeSalesProduct,NULL);
+
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+
+    return;
+}
+
+void payment()
+{
+    printf("\nDo you want to pay now or later on? y/n : ");
+
+    char ch;
+    scanf(" %c",&ch);
+
+    if(ch == 'y')
+    {
+        int uid;
+
+        printf("\nEnter your userID: ");
+        scanf("%d",&uid);
+
+        u.payment = 0;
+        
+        pthread_mutex_lock(&lock);
+        userpurchase = fopen("userpurchase.txt","r");
+
+        if(!userpurchase)
+        {
+            printf("No product is sold\n");
+            pthread_mutex_unlock(&lock);
+            return;
+        }
+
+        while(fscanf(userpurchase, "%d %s %d %f",&u.id,u.p.name,&u.p.quantity,&u.total) != EOF)
+        {
+            if(uid == u.id)
+            {
+                u.payment += u.total;
+            }
+        }
+
+        rewind(userpurchase);
+
+        printf("Payment amount is %.2f\n",u.payment);
+        sleep(1);
+        printf("Payment is processing...\n");
+        sleep(1);
+        printf("Payment successful...\n");
+
+        payment_done = 1;
+
+        usertemp = fopen("usertemp.txt","w");
+        
+        if(!usertemp)
+        {
+            printf("A simple error occured\n");
+            return NULL;
+        }
+
+        while(fscanf(userpurchase, "%d %s %d %f",&u.id,u.p.name,&u.p.quantity,&u.total) != EOF)
+        {
+            if(uid != u.id)
+            {
+                fprintf(usertemp, "%d %s %d %.2f\n", u.id, u.p.name, u.p.quantity, u.total);
+            }
+        }
+
+        fclose(userpurchase);
+        fclose(usertemp);
+
+        remove("userpurchase.txt");
+        rename("usertemp.txt","userpurchase.txt");
+
+        pthread_mutex_unlock(&lock);
+        return;
+    }
+    else if(ch == 'n')
+    {
+        printf("Your payment is pending...\nPurchasing is terminated");
+        adminView();
+        return;
+    }
+}
+
+void* updateInventory(void* args)
+{
+    pthread_mutex_lock(&lock);
+    if (!payment_done) 
+    {
+        pthread_mutex_unlock(&lock);
+        pthread_exit(NULL);
+    }
+    
+    inventory = fopen("inventory.txt", "r");
+    temp = fopen("temp.txt", "w");
+    
+    while (fscanf(inventory, "%d %s %d %f",&p.id, p.name, &p.quantity, &p.price) != EOF) {
+        if (p.id == u.p.id)
+        {
+            p.quantity -= u.p.quantity;
+        }
+
+        fprintf(temp, "%d %s %d %.2f\n", p.id, p.name, p.quantity, p.price);
+    }
+
+    fclose(inventory);
+    fclose(temp);
+
+    remove("inventory.txt");
+    rename("temp.txt", "inventory.txt");
+
+    return NULL;
+}
+
+void* storeSalesProduct(void* arg) {
+
+    pthread_mutex_lock(&lock);
+    if (!payment_done) 
+    {
+        pthread_mutex_unlock(&lock);
+        pthread_exit(NULL);
+    }
+
+    sales = fopen("sales.txt", "a");
+    fprintf(sales, "%d %s %d %.2f\n",u.id, u.p.name, u.p.quantity, u.payment);
+    fclose(sales);
+
+    pthread_mutex_unlock(&lock);
+    pthread_exit(NULL);
+}
+
+void viewSales()
+{
+    sales = fopen("sales.txt","r");
+    if(!sales)
+    {
+        printf("There is no sales statement for today.\n");
+        return;
+    }
+
+    printf("UserID\tProductName\tQuantity\tTotal\n");
+    while(fscanf(sales, "%d %s %d %f",&u.id, u.p.name, &u.p.quantity, &u.payment) != EOF)
+    {
+        printf("%d\t%s\t%d\t%.2f\n",u.id,u.p.name,u.p.quantity,u.payment);
+    }
+    fclose(sales);
+
+    return;
+}
+
 void setColor(WORD color)
 {
     SetConsoleTextAttribute(h,color);
@@ -336,16 +617,22 @@ void setColor(WORD color)
 
 void adminView()
 {
+    setColor(FOREGROUND_GREEN);
+
     printf("\nAdmin Pannel\n");
     printf("\n1-- Add product");
     printf("\n2-- Update Product");
     printf("\n3-- View Product");
     printf("\n4-- Search Product");
     printf("\n5-- Delete Product");
+    printf("\n6-- Add to Cart");
+    printf("\n7-- Per day sale");
     printf("\n0-- Exit");
-    printf("\nEnter your choice: ");
+
     int ch;
+    printf("\nEnter your choice: ");
     scanf("%d",&ch);
+
     if(ch == 1)
     {
         addProduct();
@@ -371,30 +658,94 @@ void adminView()
         deleteProduct();
         adminView();
     }
+    else if(ch == 6)
+    {
+        purchaseProduct();
+        adminView();
+    }
+    else if(ch == 7)
+    {
+        viewSales();
+        adminView();
+    }
     else if(ch == 0)
     {
+        mainMenu();
         return;
     }
     else
     {
         printf("You have chosen a wrong choice...\n");
+        mainMenu();
         return;
     }
 }
+
+void userView()
+{
+    setColor(FOREGROUND_GREEN);
+
+    printf("\nUser Pannel\n");
+    printf("\n1-- View Product");
+    printf("\n2-- Search Product");
+    printf("\n3-- Add to Cart");
+    printf("\n4-- View per day sales: ");
+    printf("\n0-- Exit");
+
+    int ch;
+    printf("\nEnter your choice: ");
+    scanf("%d",&ch);
+
+    if(ch == 1)
+    {
+        viewProduct();
+        userView();
+    }
+    else if(ch == 2)
+    {
+        searchProduct();
+        userView();
+    }
+   
+    else if(ch == 3)
+    {
+        purchaseProduct();
+        userView();
+    }
+    else if(ch == 4)
+    {
+        viewSales();
+        userView();
+    }
+    else if(ch == 0)
+    {
+        mainMenu();
+        return;
+    }
+    else
+    {
+        printf("You have chosen a wrong choice...\n");
+        mainMenu();
+        return;
+    }
+}
+
 void adminLog()
 {
     int tracking = 2;
+
     while(tracking--)
     {
         setColor(FOREGROUND_GREEN);
 
-        printf("\nEnter admin username : ");
         char username[20];
+        printf("\nEnter admin username : ");
+
         fgets(username,sizeof(username),stdin);
         username[strcspn(username,"\n")] = '\0';
 
-        printf("\nEnter admin password : ");
         char password[20];
+        printf("\nEnter admin password : ");
     
         inputPassword(password,sizeof(password));
 
@@ -402,7 +753,7 @@ void adminLog()
 
         if(logfile == NULL)
         {
-            printf("\nFailed to open file...\n");
+            printf("\nFailed to open logfile...\n");
             mainMenu();
             return;
         }
@@ -422,7 +773,7 @@ void adminLog()
         {
             setColor(FOREGROUND_BLUE);
             printf("\nlog in successful");
-            Sleep(1000);
+            sleep(1);
             adminView();
             return;
         }
@@ -430,13 +781,14 @@ void adminLog()
         {
             setColor(FOREGROUND_RED | FOREGROUND_GREEN);
             printf("\nLog in failed... You have %d more chance... \nTry again.\n",tracking);
-            Sleep(1000);        
+            sleep(1);        
         }
     }
+
     setColor(FOREGROUND_RED);
     printf("\nOOPS!!!!!...\nYou crossed your try limit... \nNow you are kicking off on your butt , go to sleep!!!\n");
     
-    Sleep(1000);
+    sleep(1);
     setColor(FOREGROUND_GREEN);
     printf("\nYou wanna change log info? y/n : ");
 
@@ -449,12 +801,12 @@ void adminLog()
     {
         settingAdminLogInfo();
         printf("\nLog in information updated successfully...\n");
-        Sleep(1500);
+        sleep(1.5);
         mainMenu();
     }
     else
     {
-        Sleep(1500);
+        sleep(1.5);
         mainMenu();
         return;
     }
@@ -501,23 +853,25 @@ void settingAdminLogInfo()
 
     if(logfile == NULL)
     {
-        printf("Failed to open file");
+        printf("Failed to open logfile");
         mainMenu();
         return;
     }
 
     char username[20];
     char password[10];
+
     printf("Enter new username : ");
     fgets(username,sizeof(username),stdin);
     username[strcspn(username,"\n")] = '\0';
 
     printf("Enter new password : ");
-
     inputPassword(password,sizeof(password));
 
     fprintf(logfile,"%s\n%s\n",username,password);
     fclose(logfile);
+    
+    return;
 }
 
 void userLog()
@@ -534,8 +888,9 @@ void userLog()
             mainMenu();
             return;
         }
-        printf("\nEnter your username: ");
         char username[20];
+        printf("\nEnter your username: ");
+
         fgets(username,sizeof(username),stdin);
         username[strcspn(username,"\n")] = '\0';
 
@@ -558,31 +913,36 @@ void userLog()
         {
             setColor(FOREGROUND_BLUE);
             printf("\nlog in successful");
-            Sleep(1000);
+
+            sleep(1);
+            userView();
             return;
         }
         else
         {
             setColor(FOREGROUND_RED | FOREGROUND_GREEN);
             printf("\nLog in failed... You have %d more chance... \nTry again.\n",tracking);
-            Sleep(1000);
+            sleep(1);
         }
     }
+
     setColor(FOREGROUND_RED);
     printf("\nOOPS!!!!!...\nYou crossed your try limit... \nNow you are kicking off on your butt , go to sleep!!!\n");
-    Sleep(1000);
+    sleep(1);
     mainMenu();
 }
 
 void mainMenu()
 {
     setColor(FOREGROUND_GREEN);
+
     printf("\n1-- admin login\n");
     printf("2-- user login\n");
     printf("0-- Exit\n");
     
-    printf("\nEnter your choice: ");
     int ch;
+    printf("\nEnter your choice: ");
+
     scanf("%d",&ch);
     fflush(stdin);
     
@@ -601,9 +961,11 @@ void mainMenu()
     }
 
 }
+
 int main()
 {   
     h = GetStdHandle(STD_OUTPUT_HANDLE);
+    
     setColor(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 
     pthread_mutex_init(&lock,NULL);
