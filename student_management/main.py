0b9011d83e46c382cd111f38c7b7458fdaeb8da5
Sumon-Ahmed <sumonahmed72953@gmail.com>
2026-01-06 13:03:53 +0600

    Implemented update,delete and search operation

--- student_management/main.py
+++ student_management/main.py
@@ -1,24 +1,34 @@
 from operations import *
 
 def menu():
-    print("\n=================================\n")
-    print("===STUDENT MANAGEMENT SYSTEM====")
-    print("\n=================================\n")
+    while(1):
+        print("\n=================================\n")
+        print("===STUDENT MANAGEMENT SYSTEM====")
+        print("\n=================================\n")
 
-    print("1-- Add student")
-    print("\n2-- View Student")
-    print("\n3-- Exit")
+        print("1-- Add student")
+        print("2-- View Student")
+        print("3-- Update Student")
+        print("4-- Delete Student")
+        print("5-- Search Student")
+        print("0-- Exit")
 
-    ch = int(input("Choose your option: "))
+        ch = int(input("\nChoose your option: "))
 
-    if(ch == 1):
-        add_student()
-    elif(ch == 2):
-        view_student()
-    elif(ch == 3):
-        return
-    else:
-        print("Invalid choice...\n")
-        return
+        if(ch == 1):
+            add_student()
+        elif(ch == 2):
+            view_student()
+        elif(ch == 3):
+            update_student()
+        elif(ch == 4):
+            delete_student()
+        elif(ch == 5):
+            search_student()
+        elif(ch == 0):
+            break
+        else:
+            print("Invalid choice...\n")
+            return
     
 menu()
--- student_management/operations.py
+++ student_management/operations.py
@@ -24,10 +24,13 @@ def add_student():
         cursor.execute(sql, values)
         mydb.commit()
 
-        print("Student added successfully\n")
+        print("\nStudent added successfully\n")
 
     except Exception as e:
         print("Error ", e)
+    finally:
+        cursor.close()
+        mydb.close()
 
 def view_student():
     try:
@@ -47,11 +50,119 @@ def view_student():
         cursor.execute(sql, values)
         results = cursor.fetchall()
 
-        print("\nStudent list \n")
+        print("\n                  Student lists :")
+        print("                 -----------------")
 
         for std in results:
             print(std)
 
     except Exception as e:
         print("Error ", e)
+    finally:
+        cursor.close()
+        mydb.close()
 
+def update_student():
+    try:
+        mydb = get_connection()
+        cursor = mydb.cursor()
+
+        roll = int(input("Enter roll no for update: "))
+        cursor.execute("SELECT * FROM STUDENT WHERE ROLL = %s",(roll,))
+        if not cursor.fetchone():
+            print("Student not found!!!\n")
+            return
+            
+        print("You can update only 2 options\n")
+        print("1-- Name,Age\n2-- Phone,Email")
+
+        ch = int(input("choose any: "))
+
+        if(ch == 1):
+            nfname = input("\nEnter new first_name: ")
+            nlname = input("\nEnter new last_name: ")
+            nage = int(input("Enter new age: "))
+
+            sql = """UPDATE STUDENT SET first_name=%s,last_name = %s, age=%s WHERE ROLL = %s"""
+            values = (nfname,nlname,nage,roll)
+
+            cursor.execute(sql,values)
+            mydb.commit()
+            print("Updated successfully for roll no : ",roll)
+
+        elif(ch == 2):
+            nphone = int(input("\nEnter new phone number: "))
+            nemail = input("\nEnter new email : ")
+
+            sql = "UPDATE STUDENT SET PHONE = %s, EMAIL = %s WHERE ROLL = %s"
+            values = (nphone,nemail,roll)
+
+            cursor.execute(sql,values)
+            mydb.commit()
+
+            print("\nUpdated successfully for roll no : ",roll)
+        else:
+            print("\nWrong entry...")
+    except Exception as e:
+        print("Error: ",e)
+    finally:
+        cursor.close()
+        mydb.close()
+
+def delete_student():
+    try:
+        mydb = get_connection()
+        cursor = mydb.cursor()
+
+        roll = int(input("Enter roll no for deletion: "))
+        ch = input("Are you sure to delete(y/n) ?? ")
+
+        if ch.lower() == 'n':
+            print("\nCancelled...\n")
+            return
+
+        cursor.execute("DELETE FROM STUDENT WHERE ROLL = %s",(roll,))
+        mydb.commit()
+
+        print("\nDeleted successfully for roll no ",roll)
+
+    except Exception as e:
+        print("Error : ",e)
+    finally:
+        cursor.close()
+        mydb.close()
+
+def search_student():
+    try:
+        mydb = get_connection()
+        cursor = mydb.cursor()
+
+        ch = int(input("\n1-- first_name\n2-- last_name\nChoose your option to search :"))
+
+        if( ch == 1):
+            fname = input("Enter first name to search : ")
+            cursor.execute("SELECT * FROM STUDENT WHERE FIRST_NAME = %s",(fname,))
+
+        elif(ch == 2):
+            lname = input("Enter last name to search : ")
+            cursor.execute("SELECT * FROM STUDENT WHERE LAST_NAME = %s",(lname,))
+        else:
+            print("\nInvalid choice...\n")
+            return
+        
+        result = cursor.fetchall()
+        if not result:
+            print("\nStudent not found...\n")
+            return
+        else:
+            print("\n                  Student lists :")
+            print("                 -----------------")
+            for row in result:
+                print(row)
+
+    except Exception as e:
+        print("Error : ",e)
+    finally:
+        cursor.close()
+        mydb.close()
+        
